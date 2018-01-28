6a546773c4dd7eab360e1c0c20fcde247fb865b6
Beta Faccion <betafcc@gmail.com>
2018-01-28 12:01:13 -0200

    Bumps version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='scrapetools',
-      version='0.2.5',
+      version='0.2.6',
       description='Web Scraping utility',
       author='Beta Faccion',
       author_email='betafcc@gmail.com',
